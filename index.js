fece18512f13d0a06978f97ba2b1b6b5e417b95b
Vsdestroyer0 <ggmendoza2005@gmail.com>
2025-11-28 11:48:17 -0600

    Cambios en axios

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const Alexa = require('ask-sdk-core');
+const axios = require('axios'); // Mover aquí para que esté disponible globalmente
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
